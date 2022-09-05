adb3fc882b018b2563367808d5639d7bcde242e6
Arash Yalpani <arash@yalpani.de>
2022-09-05 20:05:28 +0200

    serverUri => https://app.lisd.io/

--- constants.ts
+++ constants.ts
@@ -1,5 +1,4 @@
-export const SERVER_URI_NETLIFYLISD5 =
-  'https://main--resonant-pony-d9449f.netlify.app/';
+export const SERVER_URI_NETLIFYLISD5 = 'https://app.lisd.io/';
 
 export const SERVER_URI_LAPTOP = 'http://192.168.178.51:3000/';
 export const SERVER_URI_ARASH = 'http://192.168.178.60:8888/';
